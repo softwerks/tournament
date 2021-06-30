2b5ae926606eef4ec3ff800d4670defb284611da
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2021-06-29 18:02:55 -0700

    Create matches when players are in the queue.

--- tournament/matchmaker.py
+++ tournament/matchmaker.py
@@ -14,30 +14,61 @@
 
 import asyncio
 import logging
+import secrets
 import time
+
+import aioredis
+
+from . import redis
 
 logger: logging.Logger = logging.getLogger(__name__)
 
 INTERVAL: int = 5
+STARTING_POSITION_ID = "4HPwATDgc/ABMA"
+STARTING_MATCH_ID = "cAgAAAAAAAAA"
 
 
 async def run() -> None:
     try:
         while True:
             sleep_task: asyncio.Task = asyncio.create_task(asyncio.sleep(INTERVAL))
-            match_task: asyncio.Task = asyncio.create_task(match())
+            match_task: asyncio.Task = asyncio.create_task(_match())
             await asyncio.gather(sleep_task, match_task)
     except asyncio.CancelledError:
         sleep_task.cancel()
         match_task.cancel()
 
 
-async def match() -> None:
+async def _match() -> None:
     try:
         start: float = time.perf_counter()
-        await asyncio.sleep(1)
+        await _find_match()
         elapsed: float = time.perf_counter() - start
         if elapsed > INTERVAL:
-            logger.warning(f"match loop took {round(elapsed, 1)}s to execute")
+            logger.warning(f"match took {round(elapsed, 1)}s to execute")
     except asyncio.CancelledError:
         pass
+
+
+async def _find_match() -> None:
+    conn: aioredis.Redis = await redis.get_connection()
+
+    await conn.sunionstore("tournament:clone", "tournament:queue")
+
+    while await conn.scard("tournament:clone") > 1:
+        pipeline: aioredis.commands.transaction.MultiExec = conn.multi_exec()
+
+        game_id: str = secrets.token_urlsafe(9)
+
+        pipeline.hset(f"game:{game_id}", "position", STARTING_POSITION_ID)
+        pipeline.hset(f"game:{game_id}", "match", STARTING_MATCH_ID)
+
+        for i, player in enumerate(
+            await conn.spop("tournament:clone", 2, encoding="utf-8")
+        ):
+            pipeline.srem("tournament:queue", player)
+            pipeline.hset(f"game:{game_id}", f"player_{i}", player)
+            pipeline.hset(f"session:{player}", "game_id", game_id)
+            pipeline.publish(f"tournament:{player}", game_id)
+
+        await pipeline.execute()
